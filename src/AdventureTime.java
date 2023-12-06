96f05c482e4f25d2416fad3860b67aee6abe3c31
perlielui <perliel2026@student.cis.edu.hk>
2023-12-06 21:53:53 +0800

    done!

--- src/AdventureTime.java
+++ src/AdventureTime.java
@@ -5,11 +5,17 @@ public class AdventureTime {
 
     /** This is the main method and it is where you will test your implementations for challengeOne, challengeTwo, etc.
      *
+     *
+     *
+     *
      * @param args
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-
+        System.out.println(challengeOne("inputOneTwo.txt"));
+        System.out.println(challengeTwo("inputOneTwo.txt"));
+        System.out.println(challengeThree("inputThreeFour.txt"));
+        System.out.println(challengeFour("inputThreeFour.txt"));
 
     }
 
@@ -22,7 +28,21 @@ public class AdventureTime {
      * @throws IOException
      */
     public static int challengeOne(String fileName) throws IOException {
-        return 0;
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        int lines = countLinesInFile(fileName);
+        int[] str = new int[lines];
+        int in = 0;
+        int increase = 0;
+        while (scanner.hasNextLine()) {
+            str[in++] = scanner.nextInt();
+        }
+        for (int i = 1; i < str.length; i++) {
+            if (str[i] > str[i - 1]) {
+                increase++;
+            }
+        }
+        return increase;
     }
 
     /** TODO 2
@@ -34,7 +54,25 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeTwo(String fileName) throws FileNotFoundException {
-        return 0;
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        int[] array = new int[countLinesInFile(fileName)];
+        int in = 0;
+        while (scanner.hasNextLine()) {
+            array[in++] = scanner.nextInt();
+        }
+        int last = array[0] + array[1] + array[2];
+        int now;
+        int result = 0;
+        for (int i = 1; i < array.length - 3; i++) {
+            now = array[i] + array[i + 1] + array[i + 2];
+            if (now > last) {
+                result++;
+            }
+            last = now;
+
+        }
+        return result;
     }
 
     /** TODO 3
@@ -46,7 +84,29 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeThree(String fileName) throws FileNotFoundException {
-        return 0;
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        String[] array = new String[countLinesInFile(fileName)];
+        int in = 0;
+        while (scanner.hasNextLine()) {
+            array[in++] = scanner.nextLine();
+        }
+        char c;
+        int i;
+        int horizontal = 0;
+        int depth = 0;
+        for (String a : array) {
+            c = a.charAt(a.length() - 1);
+            i = Character.getNumericValue(c);
+            if (a.contains("up"))
+                depth -= i;
+            else if (a.contains("down")) {
+                depth += i;
+            } else {
+                horizontal += i;
+            }
+        }
+        return horizontal * depth;
     }
 
     /** TODO 4
@@ -58,7 +118,31 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeFour(String filename) throws FileNotFoundException {
-        return 0;
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        String[] array = new String[countLinesInFile(filename)];
+        int in = 0;
+        while (scanner.hasNextLine()) {
+            array[in++] = scanner.nextLine();
+        }
+        char c;
+        int i;
+        int aim = 0;
+        int depth = 0;
+        int horizontal = 0;
+        for (String a : array) {
+            c = a.charAt(a.length() - 1);
+            i = Character.getNumericValue(c);
+            if (a.contains("up"))
+                aim -= i;
+            else if (a.contains("down")) {
+                aim += i;
+            } else {
+                horizontal += i;
+                depth += i * aim;
+            }
+        }
+        return horizontal * depth;
     }
 
     /** This method will write the values passed as challengeOne, challengeTwo, challengeThree, and challengeFour to a text file.
